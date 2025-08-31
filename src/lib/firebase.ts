048b3fd013625db0b434c7e94cee1b75eef426c3
Andres Arenas <andriw034@gmail.com>
2025-08-31 04:17:23 +0000

    Error: [2025-08-31T04:15:55.020Z]  @firebase/firestore: "Firestore (10.1

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,9 +1,8 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-import { initializeFirestore, connectFirestoreEmulator, getFirestore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 import { config } from 'dotenv';
 
 config();
@@ -23,22 +22,6 @@ const firebaseConfig = {
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const storage = getStorage(app);
-
-// Use getFirestore() to avoid multiple instances in Next.js server/client components
 const db = getFirestore(app);
-
-if (process.env.NODE_ENV === 'development' && typeof window !== 'undefined') {
-    // This check is important to run this code only in the browser
-    // and in development mode
-    console.log("Development environment (client-side): Connecting to emulators");
-    try {
-        connectAuthEmulator(auth, "http://localhost:9099", { disableWarnings: true });
-        connectFirestoreEmulator(db, "localhost", 8080);
-    } catch (e) {
-        console.error("Error connecting to emulators. Are they running?", e);
-    }
-} else if (process.env.NODE_ENV === 'development') {
-    console.log("Development environment (server-side): Emulators will be connected on client.");
-}
 
 export { app, db, auth, storage };
