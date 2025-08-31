e3985b674b1bc16820bb5e2ff82f110cf7343e6a
Andres Arenas <andriw034@gmail.com>
2025-08-31 04:40:59 +0000

    no aparse nada ninguna vista

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,17 +1,14 @@
 
 import { DashboardSidebar } from "@/components/dashboard/sidebar";
-import { PrivateRoute } from "@/components/private-route";
 import { ReactNode } from "react";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
-    <PrivateRoute>
-      <div className="grid min-h-[calc(100vh-4rem)] md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-        <DashboardSidebar />
-        <main className="flex w-full flex-col">
-          {children}
-        </main>
-      </div>
-    </PrivateRoute>
+    <div className="grid min-h-[calc(100vh-4rem)] md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <DashboardSidebar />
+      <main className="flex w-full flex-col">
+        {children}
+      </main>
+    </div>
   );
 }
